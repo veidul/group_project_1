de0226bdd7675b9258536064f3f9b6604845e2fa
ludie lambright <ludiefletcher@aol.com>
2021-10-28 21:34:20 -0700

    wrote function to split deck to cpu hands

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,22 @@
 $(document).ready(function () {
 
     var url = "http://deckofcardsapi.com/api/deck/new/";
-    var deckID = '00fn7jnq5a7b';
+    var deckId;
+
+
+
+    var pullCard = "https://deckofcardsapi.com/api/deck/" + deckId + "/draw/?count=2"
+    var splitPlayerHand = "https://deckofcardsapi.com/api/deck/" + deckId + "/draw/?count=26"
+    // var cpuHandApi = "https://deckofcardsapi.com/api/deck/" + deckId + "/pile/cpuHand/add/?cards=" + data.cards.code
+    var cpuPlayPile = "https://deckofcardsapi.com/api/deck/" + deckId + "/pile/cpuPlayPile/add/"
+    var cpuWinnings = "https://deckofcardsapi.com/api/deck/" + deckId + "/pile/cpuWinnings/add/"
+    var playerHand = "https://deckofcardsapi.com/api/deck/" + deckId + "/pile/playerHand/add/"
+    var playerPlayPile = "https://deckofcardsapi.com/api/deck/" + deckId + "/pile/playerPlayPile/add/"
+    var playerWinnings = "https://deckofcardsapi.com/api/deck/" + deckId + "/pile/playerWinnings/add/"
     
     function getApi() {
         fetch(url)
-        .then(function (response){
+        .then(function (response) {
             if (response.ok) {
                 console.log(response);
                 response.json().then(function (deckID) {
@@ -13,7 +24,57 @@ $(document).ready(function () {
                 })
             }
         }
-        )
+            )
+        }
+
+        function newDeck(){
+        fetch("https://deckofcardsapi.com/api/deck/new/shuffle/")
+        .then(function(res){
+            return res.json()
+        }).then(function(data){
+            console.log("deck", data);
+            deckId = data.deck_id;
+            splitDeck();
+        })
     }
-getApi()
-})
+
+    
+    // getApi();
+    function splitDeck() {
+        var splitCpuHand = "https://deckofcardsapi.com/api/deck/" + deckId + "/draw/?count=26"
+        fetch(splitCpuHand)
+        // need to get the deck_id added before this will run
+        .then(function (response) {
+            if (response.ok) {
+                console.log(response);
+                //build new url based on response;
+                response.json().then(function (res) {
+                    // console.log(cpuHandPile)
+                    // fetch(cpuHandApi)
+                    //     .then(function (res) {
+                        
+                        console.log(res);
+                        var cpuCodes = res.cards.map(function(item){
+                            return item.code
+                        }).join(",");
+                        
+                       var cpuHandApi = "https://deckofcardsapi.com/api/deck/" + deckId + "/pile/cpuHand/add/?cards=" + cpuCodes
+                                    console.log(cpuCodes)
+                                    fetch(cpuHandApi)
+                                    .then(function(res){
+                                        return res.json()
+                                    })
+                                    .then(function(cpuHandData){
+                                        console.log(cpuHandData)
+                                    })
+
+                                    // response.json().then(function (cpuHand) {
+                                    //     console.log(cpuHand)
+                    })
+                }
+            })
+
+    }
+    document.addEventListener("click", newDeck);
+});
+
