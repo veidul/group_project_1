59d8f8f6ff1f16ea9977e8526755b80d6380a1e9
ludie lambright <ludiefletcher@aol.com>
2021-10-30 10:01:07 -0700

    finished split deck function

--- assets/script.js
+++ assets/script.js
@@ -73,6 +73,38 @@ $(document).ready(function () {
                     })
                 }
             })
+            var splitPlayerHand = "https://deckofcardsapi.com/api/deck/" + deckId + "/draw/?count=26"
+        fetch(splitPlayerHand)
+        // need to get the deck_id added before this will run
+        .then(function (response) {
+            if (response.ok) {
+                console.log(response);
+                //build new url based on response;
+                response.json().then(function (resu) {
+                    // console.log(cpuHandPile)
+                    // fetch(cpuHandApi)
+                    //     .then(function (res) {
+                        
+                        console.log(resu);
+                        var playerCodes = resu.cards.map(function(item){
+                            return item.code
+                        }).join(",");
+                        
+                       var playerHandApi = "https://deckofcardsapi.com/api/deck/" + deckId + "/pile/cpuHand/add/?cards=" + playerCodes
+                                    console.log(playerCodes)
+                                    fetch(playerHandApi)
+                                    .then(function(resu){
+                                        return resu.json()
+                                    })
+                                    .then(function(playerHandData){
+                                        console.log(playerHandData)
+                                    })
+
+                                    // response.json().then(function (cpuHand) {
+                                    //     console.log(cpuHand)
+                    })
+                }
+            })
 
     }
     document.addEventListener("click", newDeck);
